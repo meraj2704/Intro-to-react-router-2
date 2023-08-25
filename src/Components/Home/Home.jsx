cd138dbd71726c8762a8708ada67a237e06b71a2
IJE2704 <mh1669101@gmail.com>
2023-08-25 20:36:52 +0600

    router add and also some components

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from '../Header/Header';
+import { Outlet } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
-            
+            <Header></Header>
+            <h1>this is home page</h1>
+            <Outlet></Outlet>
         </div>
     );
 };
